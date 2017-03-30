6a06239347ec6dbb1bde4975b7d12934c814b090
Will Gant <w_gant@yahoo.co.uk>
2017-03-30 07:54:34 +0000

    Clean up ignored files

--- directory.rb
+++ directory.rb
@@ -1,16 +1,4 @@
-@students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november, age: 50, nationality: :US},
-    {name: "Darth Vader", cohort: :november, age: 62, nationality: :US},
-    {name: "Nurse Ratched", cohort: :april, age: 45, nationality: :US},
-    {name: "Michael Corleone", cohort: :march, age: 70, nationality: :Italian},
-    {name: "Alex DeLarge", cohort: :march, age: 60, nationality: :UK},
-    {name: "The Wicked Witch of the West", cohort: :november, age: 60, nationality: :US},
-    {name: "Terminator", cohort: :november, age: 35, nationality: :US},
-    {name: "Freddy Krueger", cohort: :october, age: 45, nationality: :US},
-    {name: "The Joker", cohort: :november, age: 30, nationality: :US},
-    {name: "Joffrey Baratheon", cohort: :november, age: 15, nationality: :UK},
-    {name: "Norman Bates", cohort: :november, age: 32, nationality: :US}
-    ]
+@students = []
 
 def print_header
 puts "The students of Villains Academy".center(150)
@@ -136,6 +124,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
 end
 
@@ -152,7 +141,9 @@ def process(selection)
         when "2"
             show_students
         when "3"
-            save_students    
+            save_students  
+        when "4"
+            load_students
         when "9"
             exit
         else
@@ -168,6 +159,15 @@ def save_students
        student_data = [student[:name], student[:cohort]]
        csv_line = student_data.join(",")
        file.puts csv_line
+    end
+    file.close
+end
+
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+        name, cohort = line.chomp.split(',')
+        @students << {name: name, cohort: cohort.to_sym}
     end
     file.close
 end
